fb39f55b82f0984a81cc76f79eab93938d89e7a6
EMILIO CICCONE <63019934+3394424051@users.noreply.github.com>
2026-08-21 18:43:03 +0200

    Create MASTER_STUDY.R

---
+++ MASTER_STUDY.R
@@ -0,0 +1,21 @@
+rm(list = ls())
+
+source("01_data_preparation.R")
+
+source("02_markowitz.R")
+
+source("03_black_litterman.R")
+
+source("04_qaoa.R")
+
+source("05_vqe.R")
+
+source("06_walkforward.R")
+
+source("07_montecarlo.R")
+
+source("08_statistics.R")
+
+source("09_plots.R")
+
+cat("\nSTUDIO COMPLETATO\n")
